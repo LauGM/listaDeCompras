1d7adb1c5ac0758d3558336ed3606fe6ef7d1bcf
Lau G.M <lmartin@animac.com.ar>
2022-12-29 15:25:44 -0300

    selector prioridad

--- src/components/ListaCompras.vue
+++ src/components/ListaCompras.vue
@@ -1,35 +1,44 @@
 <script setup>
-    import { ref, computed, onBeforeMount, onBeforeUnmount, onUpdated } from 'vue';
+import { ref, computed, onBeforeMount, onBeforeUnmount, onUpdated } from 'vue';
 
-    const encabezado = "Lista de compras";
-    const nuevo=ref("");
-    const lista=ref([]);
-    function agregar(){
-        lista.value.push(nuevo.value);
-        nuevo.value="";
-    }
-    function eliminar(index){
-        lista.value.splice(index,1);
-    }
-    const calcularCaracteres=computed(()=>nuevo.value.length);
-    onBeforeMount(()=>{
-        console.log("antes")
-        lista.value=JSON.parse(localStorage.getItem("lista"))||[];
-    })
-    onUpdated(()=>{
-        console.log("despues")
-        localStorage.setItem("lista",JSON.stringify(lista.value));
-    });
+const encabezado = "Lista de compras";
+const nuevo = ref("");
+const lista = ref([]);
+const prioridad=ref("");
+function agregar() {
+    lista.value.push(nuevo.value + prioridad.value);
+    nuevo.value = "";
+    prioridad.value="";
+}
+function eliminar(index) {
+    lista.value.splice(index, 1);
+}
+const calcularCaracteres = computed(() => nuevo.value.length);
+onBeforeMount(() => {
+    console.log("antes")
+    lista.value = JSON.parse(localStorage.getItem("lista")) || [];
+})
+onUpdated(() => {
+    console.log("despues")
+    localStorage.setItem("lista", JSON.stringify(lista.value));
+});
 </script>
 
 <template>
     <div>
         <h1>{{ encabezado }}</h1>
         <input type="text" placeholder="nuevo producto" v-model="nuevo">
-        <button @click="agregar()">Agregar</button>
+        <label style="margin-left: 5px" for="prioridad">Prioridad:</label>
+        <!-- //opcional prioridad -->
+        <select v-model="prioridad" name="prioridad">
+            <option value=" 🔴">alta</option>
+            <option value=" 🟡">media</option>
+            <option selected value=" 🟢">baja</option>
+        </select>
+        <button v-show="nuevo && prioridad" @click="agregar()">Agregar</button>
         <p style="margin-top: 0; font-size:smaller;">{{ calcularCaracteres }} / 50</p>
-        <div v-for="(producto,index) of lista" :key="index">
-            <p style="display:inline-block">{{ `${index+1} - ${producto }` }}</p>
+        <div v-for="(producto, index) of lista" :key="index">
+            <p style="display:inline-block">{{ `${index + 1} - ${producto}` }}</p>
             <button style="display:inline-block" @click="eliminar(index)">X</button>
         </div>
     </div>
