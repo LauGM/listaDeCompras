ae76f57d97ea0c206a648e3f00014a8b3553f623
Lau G.M <lmartin@animac.com.ar>
2022-12-29 12:17:01 -0300

    Primer commit

---
+++ src/components/ListaCompras.vue
@@ -0,0 +1,33 @@
+<script setup>
+    import { ref, computed } from 'vue';
+
+    const encabezado = "Lista de compras";
+    const nuevo=ref("");
+    const lista=ref([]);
+    function agregar(){
+        lista.value.push(nuevo.value);
+        nuevo.value=""
+    }
+    function eliminar(index){
+        lista.value.splice(index,1);
+    }
+    const calcularCaracteres=computed(()=>nuevo.value.length);
+</script>
+
+<template>
+    <div>
+        <h1>{{ encabezado }}</h1>
+        <input type="text" placeholder="nuevo producto" v-model="nuevo">
+        <button @click="agregar()">Agregar</button>
+        <p style="margin-top: 0; font-size:smaller;">{{ calcularCaracteres }} / 50</p>
+        <div v-for="(producto,index) of lista" :key="index">
+            <p style="display:inline-block">{{ `${index} - ${producto }` }}</p>
+            <button style="display:inline-block" @click="eliminar(index)">X</button>
+        </div>
+        
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <ListaCompras />
+    </div>
+</template>
+
+<script setup>
+    import ListaCompras from '../components/ListaCompras.vue';
+</script>
+
+<style scoped>
+</style>
