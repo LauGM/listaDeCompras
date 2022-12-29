f62dd75e4226b6294cc0940a15956f3085129ce0
Lau G.M <lmartin@animac.com.ar>
2022-12-29 14:09:35 -0300

    2022

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>App desarrollada con VUE3, Vite y Vue router 4</h1>
-        <h3>Diciembre 2023</h3>
+        <h3>Diciembre 2022</h3>
         <p>Para M.L.M.</p>
     </div>
 </template>
